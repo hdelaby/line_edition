be97c1464c52c1d16c79e1e16033f78749732359
Hugo Delaby <delaby.h@gmail.com>
2017-01-31 14:26:47 +0100

    Playing with term cursor

--- src/main.c
+++ src/main.c
@@ -6,11 +6,51 @@
 /*   By: hdelaby <hdelaby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 09:51:53 by hdelaby           #+#    #+#             */
-/*   Updated: 2017/01/31 10:40:34 by hdelaby          ###   ########.fr       */
+/*   Updated: 2017/01/31 14:25:02 by hdelaby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "line_editing.h"
+#include "ft_printf.h"
+
+void	ft_dlstaddnext(t_dlist **head, t_dlist *new)
+{
+	t_dlist	*ptr;
+
+	ptr = NULL;
+	if (!new || !head)
+		return ;
+	if (*head)
+	{
+		ptr = (*head)->next;
+		(*head)->next = new;
+	}
+	new->prev = *head;
+	new->next = ptr;
+	if (ptr)
+	{
+		ptr->prev = new;
+		*head = ptr;
+	}
+	else
+		*head = new;
+}
+
+int		tc_putc(int c)
+{
+	return (write(0, &c, 1));
+}
+
+void	print_dlst(t_dlist *lst)
+{
+	tputs(tgetstr("crce", NULL), 0, &tc_putc);
+	lst = ft_dlstgethead(lst);
+	while (lst)
+	{
+		ft_putstr_fd(lst->content, 0);
+		lst = lst->next;
+	}
+}
 
 int		ft_getch()
 {
@@ -23,13 +63,23 @@ char	*line_editing(void)
 {
 	int		key_pressed;
 	int		i;
+	t_dlist	*lst;
 
+	lst = NULL;
 	i = 0;
 	while (42)
 	{
 		key_pressed = ft_getch();
-		if (key_pressed == 'p')
+		if (key_pressed == '\n')
 			break;
+		else if (key_pressed == 27)
+		{
+			tputs(tgetstr("le", NULL), 0, &tc_putc);
+			lst = lst->prev;
+		}
+		else
+			ft_dlstaddnext(&lst, ft_dlstnew(&key_pressed, 2));
+		print_dlst(lst);
 	}
 	return ("LOL");
 }
